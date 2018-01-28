0f89a9eb06f3894bff95bc6e8a894909bccec094
Yaamani <mahmoud_medhat.2008@yahoo.com>
2018-01-28 16:51:54 +0200

    Creating libgdx files
    Creating libgdx files using libgdx' setup tool

---
+++ android/src/com/yaamani/battleshield/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.yaamani.battleshield;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.yaamani.battleshield.BattleshieldGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BattleshieldGame(), config);
+	}
+}
