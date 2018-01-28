31513fa419ccb8747ceac31fa4a5f3e1cf5ab089
Yaamani <mahmoud_medhat.2008@yahoo.com>
2018-01-28 17:16:37 +0200

    Adding the constants class

--- core/src/com/yaamani/battleshield/BattleshieldGame.java
+++ core/src/com/yaamani/battleshield/BattleshieldGame.java
@@ -1,20 +1,29 @@
 package com.yaamani.battleshield;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
-public class BattleshieldGame extends ApplicationAdapter {
+public class BattleshieldGame extends Game {
 
-	
 	@Override
 	public void create () {
 
 	}
 
-	@Override
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+
+    }
+
+    @Override
 	public void render () {
 
 	}
---
+++ core/src/com/yaamani/battleshield/Utilities/Constants.java
@@ -0,0 +1,11 @@
+package com.yaamani.battleshield.Utilities;
+
+/**
+ * Created by mahmo on 28/01/2018.
+ */
+
+public class Constants {
+
+    public static final float WORLD_SIZE = 510.0f;
+
+}
