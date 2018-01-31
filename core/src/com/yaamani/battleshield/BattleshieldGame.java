1719784ca7e4d55d49c6f59e1c2dbe2534dd5d7c
Yaamani <mahmoud_medhat.2008@yahoo.com>
2018-01-31 15:58:43 +0200

    adding ExtendViewport.
    the ShapeRenderer object is just for checking .

--- core/src/com/yaamani/battleshield/BattleshieldGame.java
+++ core/src/com/yaamani/battleshield/BattleshieldGame.java
@@ -1,35 +1,83 @@
 package com.yaamani.battleshield;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import static com.yaamani.battleshield.Utilities.Constants.*;
+
 
 public class BattleshieldGame extends Game {
 
+    public static final String TAG = BattleshieldGame.class.getSimpleName();
+
+
+    private Viewport viewport;
+
+    private ShapeRenderer shapeRenderer;
+
 	@Override
 	public void create () {
+        viewport = new ExtendViewport(WORLD_SIZE, WORLD_SIZE);
 
-	}
+        shapeRenderer = new ShapeRenderer();
+        shapeRenderer.setAutoShapeType(true);
+    }
 
     @Override
     public void resize(int width, int height) {
         super.resize(width, height);
 
+        viewport.update(width, height, true);
+
+        Gdx.graphics.setTitle("Resolution = " + width + "*" + height + " | Aspect Ratio = " + ratio(width, height)); //I think I'm gonna need that
+        //Gdx.graphics.setWindowedMode(1000, 1000); //This is the code to change window size.
     }
 
     @Override
 	public void render () {
+        viewport.apply();
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        shapeRenderer.setProjectionMatrix(viewport.getCamera().combined);
+
+        shapeRenderer.begin();
+
+        shapeRenderer.set(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.SKY);
+        shapeRenderer.rect(0, 0, WORLD_SIZE, WORLD_SIZE);
+
+        //shapeRenderer.set(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.WHITE);
+        shapeRenderer.circle(viewport.getWorldWidth() / 2, viewport.getWorldHeight() / 2, WORLD_SIZE / 2.3f);
+
+        shapeRenderer.end();
 
 	}
 	
 	@Override
 	public void dispose () {
-
+        shapeRenderer.dispose();
 	}
+
+    //The next 2 methods for calculating the aspect ratio.
+	private int gcd(int p, int q) {
+        if (q == 0) return p;
+        else return gcd(q, p % q);
+    }
+
+    private String ratio(int a, int b) {
+        final int gcd = gcd(a,b);
+        return a/gcd + ":" + b/gcd;
+    }
 }
--- desktop/src/com/yaamani/battleshield/desktop/DesktopLauncher.java
+++ desktop/src/com/yaamani/battleshield/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import com.yaamani.battleshield.BattleshieldGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		//16:9
+		config.width = 800;
+		config.height = 450;
+
 		new LwjglApplication(new BattleshieldGame(), config);
 	}
 }
