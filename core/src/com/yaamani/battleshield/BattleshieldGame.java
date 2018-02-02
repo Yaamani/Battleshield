fe7fb53068ba836fc062077cb3930f93508b4779
Yaamani <mahmoud_medhat.2008@yahoo.com>
2018-02-02 13:58:55 +0200

    adding SETTINGS class to Constants

--- core/src/com/yaamani/battleshield/BattleshieldGame.java
+++ core/src/com/yaamani/battleshield/BattleshieldGame.java
@@ -55,7 +55,6 @@ public class BattleshieldGame extends ApplicationAdapter {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-
         stage.act();
         stage.draw();
 	}
--- core/src/com/yaamani/battleshield/Utilities/Constants.java
+++ core/src/com/yaamani/battleshield/Utilities/Constants.java
@@ -10,4 +10,22 @@ public class Constants {
 
     public static final float TURRET_SIZE = WORLD_SIZE / 12.5f;
 
+    public static class SETTINGS {
+
+        public static class STARS {
+            public enum EXISTENCE {ON, OFF}
+
+            public enum MOVEMENT {SIMPLE, COMPLEX}
+
+            public enum DENSITY {LOW, MEDIUM, HIGH}
+        }
+
+        public static class DEFAULTS {
+            public static final STARS.EXISTENCE STARS_EXISTENCE = STARS.EXISTENCE.ON;
+
+            public static final STARS.MOVEMENT STARS_MOVEMENT = STARS.MOVEMENT.COMPLEX;
+
+            public static final STARS.DENSITY STARS_DENSITY = STARS.DENSITY.MEDIUM;
+        }
+    }
 }
